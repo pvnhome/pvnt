ccb5f1f0bc9b58023e9eddbb043a0e2953fe712f
victor <vp@fininfo.kz>
2016-12-12 15:08:50 +0600

    Simple pvnt.bat script for Windows was added

---
+++ scripts/pvnt.bat
@@ -0,0 +1,13 @@
+@echo off
+@rem ###################################
+@rem  PVNT startup script for Windows
+@rem ###################################
+
+set DIR=%~dp0
+if "%DIR%" == "" set DIR=.\
+set APPHOME=%DIR%..
+
+set CLASSPATH=%APPHOME%\lib\pvnt-0.9.2.jar
+
+@rem Execute Gradle
+java.exe -classpath "%CLASSPATH%" kz.pvnhome.pvnt.PVNTemplateRunner %*
